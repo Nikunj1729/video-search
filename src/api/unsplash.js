944fb2edeca4bfc939a710d5cdd7416814a67326
Nikunj Dalsaniya <nikunj5390.dalsaniya@gmail.com>
2020-05-23 18:45:03 +0530

    react-create-app on Heroku

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-  baseURL: "https://api.unsplash.com/",
-  headers: {
-    Authorization: "Client-ID xY7zJJ1iGOu3KJExUHwLdjqn_ezNyp1nLVbLIVdXGLg",
-  },
-});
