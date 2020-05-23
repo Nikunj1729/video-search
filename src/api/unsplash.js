335200736986794067f50a9f4712d314ffdde6ce
Nikunj Dalsaniya <nikunj5390.dalsaniya@gmail.com>
2020-05-23 18:21:17 +0530

    Init Commit

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  headers: {},
+  baseURL: "https://api.unsplash.com/",
+  headers: {
+    Authorization: "Client-ID xY7zJJ1iGOu3KJExUHwLdjqn_ezNyp1nLVbLIVdXGLg",
+  },
 });
